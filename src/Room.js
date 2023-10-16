5a635b92f464cbd2923f3db8bbe3afbddfd9b297
Nitin_linux <nitinprabhu46@gmail.com>
2023-10-17 00:38:04 +0530

    files added

---
+++ src/Room.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    // const appID = 946219318;
+    const appID = 1802935611;
+    // const serverSecret = "8e0b853d79deae0bcbfe949b73ca46a4";
+    const serverSecret = "d951b03eb6c4a7c4ffa56b2b40ab7c7a";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Nithin_Prabhu"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
